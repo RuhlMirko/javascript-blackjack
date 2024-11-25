7775a9806aa0d5af71b90470ccdcccbb403dc7ef
Mirko <ruhlmirkojoel@gmail.com>
2024-11-25 16:07:21 -0300

    Added more probability of lower draws in the first hand

--- app.js
+++ app.js
@@ -4,34 +4,52 @@ const suits = ["diamonds", "clubs", "hearts", "spades"];
 let current_hand = 0;
 const hitBtn = document.querySelector(".btn--hit");
 
-let first_hand_img = document.querySelector(".hand--0");
-let second_hand_img = document.querySelector(".hand--1");
 const images = document.querySelectorAll("img");
+let score_p1 = document.querySelector(".score");
+let hand = 0;
+let current_score = 0;
 
-let random_array = get_new_rand();
+// Starts with two cards
+hit_player(true);
+hit_player(true);
 
-first_hand_img.src = `cards/${random_array[0]}_of_${
-  suits[random_array[1]]
-}.png`;
-random_array = get_new_rand();
-second_hand_img.src = `cards/${random_array[0]}_of_${
-  suits[random_array[1]]
-}.png`;
-
-function get_new_rand() {
-  let random_num = Math.trunc(Math.random() * 13) + 1;
-  let random_suit = Math.trunc(Math.random() * 4);
+function get_new_rand(lower) {
+  if (lower) {
+    var random_num = Math.trunc(Math.random() * 8) + 1;
+    var random_suit = Math.trunc(Math.random() * 4);
+  } else {
+    var random_num = Math.trunc(Math.random() * 13) + 1;
+    var random_suit = Math.trunc(Math.random() * 4);
+  }
   return [random_num, random_suit];
 }
 
-let hand = 2;
-function hit_player() {
+function hit_player(low = false) {
   if (hand <= 6) {
-    random_array = get_new_rand();
+    if (low) {
+      var random_array = get_new_rand(true);
+    } else {
+      var random_array = get_new_rand(false);
+    }
+
+    let card_number = random_array[0];
+    let card_suit = random_array[1];
     document.querySelector(`.hand--${hand}`).classList.remove("hidden");
-    document.querySelector(`.hand--${hand}`).src = `cards/${
-      random_array[0]
-    }_of_${suits[random_array[1]]}.png`;
+    document.querySelector(
+      `.hand--${hand}`
+    ).src = `cards/${card_number}_of_${suits[card_suit]}.png`;
+
+    if (card_number >= 10) {
+      current_score += card_number;
+      score_p1.textContent = current_score;
+    } else {
+      current_score += card_number;
+      score_p1.textContent = current_score;
+    }
+
+    if (current_score > 21) {
+      document.querySelector(".score").style.color = "red";
+    }
 
     hand += 1;
   }
